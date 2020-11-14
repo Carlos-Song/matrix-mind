18f6196db1dc4171f9ebab3f98eb06a43e115934
Carlos Wells <silverwingsw@icloud.com>
2020-11-15 04:30:30 +0800

    Add([App][Entry]): Add app entrance and config

---
+++ src/App.svelte
@@ -0,0 +1,150 @@
+<script lang="ts">
+  import Banner from "./components/Banner.svelte";
+  import Test from "./posts/test/Test.svx";
+  import {
+    Header,
+    Tabs,
+    Tab,
+    TabContent,
+    HeaderUtilities,
+    HeaderAction,
+    HeaderActionSearch,
+    HeaderGlobalAction,
+    HeaderPanelLinks,
+    HeaderPanelDivider,
+    HeaderPanelLink,
+    Content,
+    SideNavItems,
+    SideNavMenuItem,
+    SideNavMenu,
+    SideNav,
+    SideNavLink,
+    SkipToContent,
+  } from "carbon-components-svelte";
+  import SettingsAdjust20 from "carbon-icons-svelte/lib/SettingsAdjust20";
+  let isOpen = false;
+  let isSideNavOpen = false;
+
+  $: marginLeft = isSideNavOpen ? 16 : 0;
+  $: document.documentElement.style.setProperty("--mleft", marginLeft + "rem");
+</script>
+
+<style lang="scss" global>
+  /** Gray 10 theme **/
+  @import "carbon-components-svelte/css/g10";
+  #main-content {
+    padding: 0;
+  }
+
+  #main-content.bx--content {
+    margin-left: var(--mleft);
+  }
+
+  .bx--tabs__nav {
+    height: 100%;
+  }
+
+  .header-banner {
+    height: calc(var(--height) - 3rem);
+    width: 100%;
+    min-height: 250px;
+    display: flex;
+    justify-content: left;
+    align-items: flex-end;
+    padding: 2rem;
+    background-color: #000;
+    color: #fff;
+  }
+
+  .bx--side-nav__overlay {
+    z-index: 100;
+    position: fixed !important;
+  }
+
+  .tabs {
+    background-color: #000;
+  }
+
+  .bx--tabs {
+    position: sticky !important;
+    top: 3rem;
+    border-top: 1px solid #989696;
+    background-color: #000 !important;
+  }
+
+  .bx--tabs__nav-item {
+    background-color: #000 !important;
+  }
+
+  .bx--tabs__nav-link {
+    color: #fff !important;
+  }
+
+  .bx--side-nav ~ .bx--content {
+    margin-left: 0;
+  }
+
+  .content-1 {
+    height: 1000px;
+    background-color: #fff;
+  }
+
+  .haha {
+    font-size: large;
+  }
+
+  .side-nav {
+    background-color: #fff222;
+  }
+</style>
+
+<Header company="MatrixMind" platformName="Carlos" bind:isSideNavOpen>
+  <div slot="skip-to-content">
+    <SkipToContent />
+  </div>
+
+  <HeaderUtilities>
+    <HeaderActionSearch />
+    <HeaderGlobalAction aria-label="Settings" icon={SettingsAdjust20} />
+    <HeaderAction bind:isOpen>
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>用户</HeaderPanelDivider>
+        <HeaderPanelLink>登录</HeaderPanelLink>
+        <HeaderPanelDivider>合作</HeaderPanelDivider>
+        <HeaderPanelLink>关于</HeaderPanelLink>
+        <HeaderPanelLink>支持我</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+  </HeaderUtilities>
+</Header>
+
+<SideNav fixed bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavLink text="数学" />
+    <SideNavLink text="Server" />
+    <SideNavLink text="Web" />
+    <SideNavMenu text="Algorithm">
+      <SideNavMenuItem href="/" text="Link 1" />
+      <SideNavMenuItem href="/" text="Link 2" />
+      <SideNavMenuItem href="/" text="Link 3" />
+    </SideNavMenu>
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <Banner title="Python 与 数学" />
+  <Tabs type="container">
+    <Tab label="基础" />
+    <Tab label="进阶" />
+    <Tab label="应用" />
+    <div slot="content">
+      <TabContent>
+        <div class="content-1">
+          <Test />
+        </div>
+      </TabContent>
+      <TabContent>Content 2</TabContent>
+      <TabContent>Content 3</TabContent>
+    </div>
+  </Tabs>
+</Content>
---
+++ src/components/Banner.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+  import { Runtime, Inspector } from "@observablehq/runtime";
+  export var title;
+
+  function notebook(runtime, observer) {
+    const main = runtime.module();
+    main
+      .variable(observer("animation"))
+      .define("animation", ["DOM", "width", "mutable speed"], function* (
+        DOM,
+        width,
+        $0
+      ) {
+        const height = 300;
+        const min = 0;
+        const max = 100;
+        const max2 = Math.pow(max, 2);
+        const context = DOM.context2d(width, height);
+        const canvas = context.canvas;
+
+        const n = 100;
+        const particles = new Array(n);
+        for (let i = 0; i < n; ++i) {
+          particles[i] = {
+            x: Math.random() * (width + max * 2) - max,
+            y: Math.random() * (height + max * 2) - max,
+            vx: 0,
+            vy: 0,
+          };
+        }
+
+        while (true) {
+          for (let i = 0; i < n; ++i) {
+            const p = particles[i];
+            p.x += p.vx;
+            if (p.x < -max) p.x += width + max;
+            else if (p.x > width + max) p.x -= width + max;
+            p.y += p.vy;
+            if (p.y < -max) p.y += height + max;
+            else if (p.y > height + max) p.y -= height + max;
+            p.vx += $0.value * (Math.random() - 0.5) - 0.01 * p.vx;
+            p.vy += $0.value * (Math.random() - 0.5) - 0.01 * p.vy;
+          }
+
+          for (let i = 0; i < n; ++i) {
+            for (let j = i + 1; j < n; ++j) {
+              const pi = particles[i];
+              const pj = particles[j];
+              const dist = Math.pow(pi.x - pj.x, 2) + Math.pow(pi.y - pj.y, 2);
+              if (dist < max2) {
+                context.beginPath();
+                context.strokeStyle = `hsl(200,80%,${
+                  100 - ((max2 - dist) / max2) * 25
+                }%)`;
+                context.moveTo(pi.x, pi.y);
+                context.lineTo(pj.x, pj.y);
+                context.stroke();
+              }
+            }
+          }
+
+          yield canvas;
+        }
+      });
+    main.define("initial speed", function () {
+      return 0.1;
+    });
+    main
+      .variable(observer("mutable speed"))
+      .define(
+        "mutable speed",
+        ["Mutable", "initial speed"],
+        (M, _) => new M(_)
+      );
+    main
+      .variable(observer("speed"))
+      .define("speed", ["mutable speed"], (_) => _.generator);
+
+    return main;
+  }
+
+  // reference to mutable speed
+  let speed = { value: 0.1 };
+
+  function draw(node) {
+    const runtime = new Runtime();
+    runtime.module(notebook, (name) => {
+      if (name === "animation") {
+        return new Inspector(node);
+      }
+      if (name === "mutable speed") {
+        return {
+          fulfilled: (value) => {
+            speed = value;
+          },
+        };
+      }
+    });
+  }
+
+</script>
+
+<style lang="scss">
+  .banner-container {
+    position: relative;
+  }
+
+  .banner-title {
+    position: absolute;
+    bottom: 2rem;
+    left: 2rem;
+  }
+</style>
+
+<div class="banner-container">
+  <div use:draw />
+  <h1 class="banner-title">{title}</h1>
+</div>
